e4fa22db5bc95817293e30c87b3d1a3c23ce2597
Lynn Luong <lynn.h.luong@gmail.com>
2022-11-08 23:10:32 -0500

    splash done

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -15,9 +15,9 @@ function SplashPage() {
             <NavBar />
             <div className='image-container'>
                 <div className='splash-image1'>
-                    <h1>
+                    <div>
                         IMAGINE A PLACE...
-                    </h1>
+                    </div>
                     <p>
                         ...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.
                     </p>
